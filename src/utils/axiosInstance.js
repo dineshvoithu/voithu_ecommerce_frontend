9e40595f26b7739bf854bd8e1a99b9514820fee5
dineshvoithu <dineshkumarvoithu@gmail.com>
2025-07-21 19:56:18 +0530

    withcredential false

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -3,6 +3,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_URL, // your backend base URL
+  withCredentials: false,
 });
 
 // Attach the token to every request
